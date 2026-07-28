64f28028e785244b5dc6554bf1a9ffb79c29fc31
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-07-28 14:57:06 +0530

    Add package declaration and comments to SecurityApplication
    Added package declaration and comments for clarity.

--- main/java/SecurityApplication.java
+++ main/java/SecurityApplication.java
@@ -1,2 +1,7 @@
+package com.spring.security;
+//In Memory Spring Security Implementation Demo.
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class SecurityApplication {
 }
