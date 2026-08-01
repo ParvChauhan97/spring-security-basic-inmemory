eae1367e32cac44821ff8900945d54e2eca8ac7b
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-08-01 10:29:43 +0530

    Update helloController.java

--- main/java/helloController.java
+++ main/java/helloController.java
@@ -2,6 +2,7 @@ package com.spring.security;
 
 public class helloController {
 
+   @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
    @GetMapping("/hello")
    public String SayHello() {
       return "Hello Security";
