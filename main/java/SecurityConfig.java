47221d8bfeede6a509b3a583c8e59fff0f329256
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-08-11 21:34:04 +0530

    Add @EnableWebSecurity annotation to SecurityConfig

--- main/java/SecurityConfig.java
+++ main/java/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.spring.security;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
   public SecurityFilterChain securityFilterChain(HttpSecurity http){
