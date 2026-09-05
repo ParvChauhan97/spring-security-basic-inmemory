374a36f053e58d00deb54f548f42a20c717f698c
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-09-05 22:06:56 +0530

    Update SecurityConfig for admin role access

--- main/java/SecurityConfig.java
+++ main/java/SecurityConfig.java
@@ -6,12 +6,12 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-  
+
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig { 
-
+ 
 @Bean
 public SecurityFilterChain securityFilterChain(HttpSecurity http) {
   http.authorizeHttpRequests(authorizeRequests ->
