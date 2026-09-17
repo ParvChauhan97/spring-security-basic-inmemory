2e5f9b8e9ea455fa0aff20f5214179cbc9cd3399
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-09-17 22:27:47 +0530

    Update in @PreAuthorize annotation

--- main/java/helloController.java
+++ main/java/helloController.java
@@ -2,7 +2,7 @@ package com.spring.security;
 
 public class helloController {
 
-   @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
+   @PreAuthorize("hasAnyRole('ADMIN', 'USER')") 
    @GetMapping("/hello")
    public String SayHello() {
       return "Hello Security";
